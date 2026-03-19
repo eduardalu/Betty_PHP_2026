ed4c5871e382496b1491e8969bcab3f3e4e4bc61
eduardalu <eduardalusimionato@gmail.com>
2026-03-19 14:37:08 +0000

    19/03/2026

---
+++ src/recebe.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1> Recebe </h1>
+</body>
+</html>
+<?php
+echo "<pre>"; print_r($_POST); echo "</pre>";
+
+$login=$_POST["login"];
+$senha=$_POST["senha"];
+
+echo "Login: <b> $login </b><br>";
+echo "Senha: <b> $senha </b><br>";
+?>
